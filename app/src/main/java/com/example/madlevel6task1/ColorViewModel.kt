1d7f0a03549d0818f4fd512a477621876efb4660
Chris <chris.hu@hva.nl>
2021-01-08 01:15:06 +0100

    MadLevel6Task1 done

---
+++ app/src/main/java/com/example/madlevel6task1/ColorViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.madlevel6task1
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ColorViewModel : ViewModel() {
+    private val colorRepository = ColorRepository()
+
+    val colorItems: LiveData<List<ColorItem>> get() = _colorItems
+
+    private val _colorItems = MutableLiveData<List<ColorItem>>().apply {
+        value = colorRepository.getColorItems()
+    }
+}
